4ebf01750a5c23c070a2c67ee1a09bdd68aee329
SaiPraneetha703 <vdspraneetha@gmail.com>
2025-07-28 19:53:00 +0530

    updated register

--- Ecommerce/src/pages/RegisterPage.js
+++ Ecommerce/src/pages/RegisterPage.js
@@ -19,7 +19,7 @@ function RegisterPage() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('https://mern-ecom-icrv.onrender.com/register' , {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, password, phone }),
